069fe2cb9e722308c51b4a4fca4c82f1326ef5dd
Jucavi <jcarvil@gmail.com>
2022-02-08 14:15:50 +0100

    modify testing Base class

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,10 +5,19 @@ from tfm import create_app
 class BaseTestClass(unittest.TestCase):
     def setUp(self):
         self.app = create_app(environment='testing')
+        # self.ctx = self.app.app_context()
+        # self.ctx.push()
+        self.start_db()
 
-        with self.app.app_context():
-            pass
+        # with self.ctx:
+        #     pass
 
     def tearDown(self):
-        with self.app.app_context():
-            pass
+        self.start_db()
+        # with self.ctx:
+        #     pass
+
+
+    def start_db(self):
+        runner = self.app.test_cli_runner()
+        runner.invoke(args=['start-db'])
